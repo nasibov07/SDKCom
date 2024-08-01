8cce91e34683a5f93f1b81225e9e9261768501ff
Ровшан Насибов <renasibov@gid.ru>
2024-08-01 13:26:34 +0400

    feature(ios) Создание UI компанента

---
+++ UIComonants/UIComonants/UIComonants.h
@@ -0,0 +1,18 @@
+//
+//  UIComonants.h
+//  UIComonants
+//
+//  Created by Ровшан Насибов on 01.08.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIComonants.
+FOUNDATION_EXPORT double UIComonantsVersionNumber;
+
+//! Project version string for UIComonants.
+FOUNDATION_EXPORT const unsigned char UIComonantsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIComonants/PublicHeader.h>
+
+
