71de991b0c00ef8464468a8587bbd2d8016680e3
Ровшан Насибов <renasibov@gid.ru>
2024-08-06 15:36:40 +0400

    feature(iOS-44) Добавление изменений в Timer

--- UIComonants/UIComponents/Componants/SheetModifier/View/SheetModifier.swift
+++ UIComonants/UIComponents/Componants/SheetModifier/View/SheetModifier.swift
@@ -6,29 +6,29 @@
 //
 
 import SwiftUI
-
-public struct SheetModifier: ViewModifier {
-    @EnvironmentObject private var root: Root
-    
-    public func body(content: Content) -> some View {
-        ZStack {
-            content
-                .frame(maxWidth: .infinity)
-                .frame(maxHeight: .infinity)
-                .background(AppColor.AppWhite.color)
-                .foregroundColor(AppColor.AppDark.color)
-                .font(.body)
-            
-            Button(action: {}, label: {})
-                .sheet(isPresented: $root.sheetData.isShow, content: {
-                    SheetView()
-                        .presentationDetents([.fraction(0.4),.medium])
-                        .presentationDragIndicator(.automatic)
-                })
-        }
-    }
-}
-
-#Preview {
-    SheetModifier() as! any View
-}
+//
+//public struct SheetModifier: ViewModifier {
+//    @EnvironmentObject private var root: Root
+//    
+//    public func body(content: Content) -> some View {
+//        ZStack {
+//            content
+//                .frame(maxWidth: .infinity)
+//                .frame(maxHeight: .infinity)
+//                .background(AppColor.AppWhite.color)
+//                .foregroundColor(AppColor.AppDark.color)
+//                .font(.body)
+//            
+//            Button(action: {}, label: {})
+//                .sheet(isPresented: $root.sheetData.isShow, content: {
+//                    SheetView()
+//                        .presentationDetents([.fraction(0.4),.medium])
+//                        .presentationDragIndicator(.automatic)
+//                })
+//        }
+//    }
+//}
+//
+//#Preview {
+//    SheetModifier() as! any View
+//}
---
+++ UIComonants/UIComponents/Componants/Timer/View/TimerView.swift
@@ -0,0 +1,89 @@
+//
+//  TimerView.swift
+//  BaritshkaClient
+//
+//  Created by Ровшан Насибов on 31.03.2024.
+//
+
+import SwiftUI
+import Foundation
+import Combine
+
+public struct AuthTimer: View {
+    public typealias CallBackBlock = () -> Void
+    
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Binding public var countTimer: CountTimer
+    @StateObject private var watch = Watch()
+    
+    public let callBack: CallBackBlock
+    
+    public init(countTimer: Binding<CountTimer>, callBack: @escaping CallBackBlock) {
+        self._countTimer = countTimer
+        self.callBack = callBack
+    }
+    
+    public var body: some View {
+        VStack(content: {
+            HStack(content: {
+                Text(countTimer.canSend.title)
+                    .onTapGesture {
+                        if countTimer.canSend == .send {
+                            countTimer.canSend = .wait
+                            callBack()
+                        }
+                    }
+                
+                if (countTimer.canSend == .wait) {
+                    Text(watch.time)
+                }
+            })
+            .foregroundColor(countTimer.canSend.foregroundColor)
+            .font(.system(size: 14))
+            .multilineTextAlignment(.leading)
+            .frame(width: getWidth() - 40, height: 30, alignment: .center)
+        })
+        .onChange(of: watch.isActive, perform: { newValue in
+            watch.isActive == true ? (countTimer.canSend = .wait) : (countTimer.canSend = .send)
+        })
+        .onChange(of: countTimer.canSend) { newValue in
+            guard countTimer.canSend == .wait else { return }
+            watch.start(minutes: Float(countTimer.expired/60))
+        }
+        .onAppear {
+            watch.start(minutes: Float(countTimer.expired/60))
+        }
+        .onReceive(timer) { _ in
+            watch.updateCountdown()
+        }
+        .onChange(of: watch.minutes, perform: { input in
+            if watch.minutes == 0 {
+                self.countTimer.canSend = .send
+            }
+        })
+    }
+    
+    private func getHours(sec: Int) -> String {
+        let getHours = (sec / 3600)
+        return "\(getHours)"
+    }
+
+    private func getMinuts(sec: Int) -> String {
+        let getMin = (sec % 3600) / 60
+        return sec <= 3600 ? "0\(getMin)" : "\(getMin)"
+    }
+
+    private func getSec(sec: Int) -> String {
+        let getMin = (sec / 60)
+        let getSec = sec - (getMin * 60)
+        return getSec < 10 ? "0\(getSec)" : "\(getSec)"
+    }
+}
+
+struct AuthTimer_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthTimer(countTimer: .constant(.init(canSend: .blocked, canSendCount: 3, expired: 60))) {
+            print("click")
+        }
+    }
+}
