9a9478f049f165dcdfe4b6cc0f504117f4cddddd
Ровшан Насибов <renasibov@gid.ru>
2024-08-06 16:35:08 +0400

    feature(iOS-48) add-sdk-semple

---
+++ UIComonants/Semple/SempleApp.swift
@@ -0,0 +1,18 @@
+//
+//  SempleApp.swift
+//  Semple
+//
+//  Created by Ровшан Насибов on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SempleApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            MainScreen()
+        }
+    }
+}
